f570ec24bff9c87801caf54a55a1788d4c28308a
Kai Chen <kaichen120@gmail.com>
2018-10-23 23:47:22 -0700

    index.js: Update regex for hyphen rules.
    Add rules in regex for leading, trailing, and consecutive hyphens.
    
    Closes https://github.com/andrewda/valid-github-username/issues/1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = (name) => {
-  const replaceRegex = /[^a-zA-Z0-9-]/g;
+  const replaceRegex = /[^a-zA-Z0-9-]*(-{2,}?)*(-$)*(^-)*/g;
   const replaced = name.replace(replaceRegex, '');
   const shortened = replaced.substring(0, 39);
   return shortened;
